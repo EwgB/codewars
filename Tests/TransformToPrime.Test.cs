bb67c1fc485bebb61a5815d87a99d9bd4617cdb5
Ewgenij Belzmann <ewgbel@gmail.com>
2019-04-19 18:59:55 +0200

    Adds soluttion, test project and first kata solution

---
+++ Tests/TransformToPrime.Test.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TransformToPrime;
+
+namespace Tests {
+    [TestClass]
+    public class TransformToPrime_Test {
+        [TestMethod]
+        public void Test_MinimumNumber() {
+            Assert.AreEqual(1, Solution.MinimumNumber(new int[] { 3, 1, 2 }));
+            Assert.AreEqual(0, Solution.MinimumNumber(new int[] { 5, 2 }));
+            Assert.AreEqual(0, Solution.MinimumNumber(new int[] { 1, 1, 1 }));
+            Assert.AreEqual(5, Solution.MinimumNumber(new int[] { 2, 12, 8, 4, 6 }));
+            Assert.AreEqual(2, Solution.MinimumNumber(new int[] { 50, 39, 49, 6, 17, 28 }));
+        }
+    }
+}
---
+++ TransformToPrime/Solution.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace TransformToPrime {
+    /**
+     * Given a List [] of n integers , finds minimum mumber to be inserted in a list
+     * so that the sum of all elements of list should equal the closest prime number .
+    */
+    public class Solution {
+        public static int MinimumNumber(int[] numbers) {
+            var sum = numbers.Sum();
+
+            var result = 0;
+            while (!IsPrime(sum + result)) {
+                result++;
+            }
+
+            return result;
+        }
+
+        private static bool IsPrime(int num) {
+            foreach (var i in Enumerable.Range(2, (int)Math.Floor(Math.Sqrt(num)) - 1)) {
+                if (num % i == 0) {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
